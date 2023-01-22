1ac61540a299af277fc44e34a021ef085a3346a4
lukas <lukas.ikvm1@gmail.com>
2023-01-22 14:17:35 +0100

    Add gradle dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+val invoker: Configuration by configurations.creating
+
+
 plugins {
     kotlin("jvm") version "1.7.21"
+    kotlin("plugin.serialization") version "1.8.0"
+    id("com.github.johnrengelman.shadow") version "4.0.4"
     application
 }
 
@@ -12,8 +17,32 @@ repositories {
     mavenCentral()
 }
 
+
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.8.0")
+    implementation("com.google.cloud:google-cloudevent-types:0.3.0")
+
+    implementation("io.github.microutils:kotlin-logging-jvm:3.0.4")
+    implementation("org.slf4j:slf4j-api:2.0.6")
+    implementation("org.slf4j:slf4j-simple:2.0.6")
+
+    implementation("com.google.cloud.functions:functions-framework-api:1.0.4")
+    invoker("com.google.cloud.functions.invoker:java-function-invoker:1.1.0")
+
+
+    testImplementation("com.google.guava:guava-testlib:31.1-jre")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5:1.8.0")
+    testImplementation("org.jetbrains.kotlin:kotlin-test:1.8.0")
+    testImplementation ("com.google.truth:truth:1.1.3")
+    testImplementation ("org.mockito:mockito-core:4.11.0")
+
+
+}
+
+application {
+    mainClass.set("com.example.httpdeploy.AppKt")
 }
 
 tasks.test {
@@ -21,8 +50,37 @@ tasks.test {
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.jvmTarget = "11"
 }
+
+task<JavaExec>("runFunction") {
+    mainClass.set("com.google.cloud.functions.invoker.runner.Invoker")
+    classpath(invoker)
+    inputs.files(configurations.runtimeClasspath, sourceSets["main"].output)
+    args(
+        "--target",
+        project.findProperty("runFunction.target") ?: "com.example.httpdeploy.App",
+        "--port",
+        project.findProperty("runFunction.port") ?: 8080
+    )
+    doFirst {
+        args("--classpath", files(configurations.runtimeClasspath, sourceSets["main"].output).asPath)
+    }
+}
+
+
+tasks {
+    named<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJar") {
+        archiveBaseName.set("shadow")
+        mergeServiceFiles()
+        manifest {
+            attributes(mapOf("Main-Class" to "com.example.httpdeploy.App"))
+        }
+    }
+}
+
+
+
 
 application {
     mainClass.set("MainKt")
