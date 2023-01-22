31d9f8fe605cada8f11f2307a6b587975768cc68
lukas <lukas.ikvm1@gmail.com>
2023-01-22 14:19:11 +0100

    Add HttpFunction

--- src/main/kotlin/com/example/httpdeploy/Main.kt
+++ src/main/kotlin/com/example/httpdeploy/Main.kt
@@ -1,2 +1,16 @@
 package com.example.httpdeploy
 
+import com.google.cloud.functions.HttpFunction
+import com.google.cloud.functions.HttpRequest
+import com.google.cloud.functions.HttpResponse
+import java.util.logging.Logger
+
+@Suppress("unused") // Entrypoint
+
+class App : HttpFunction {
+    private val logger: Logger = Logger.getLogger(App::class.java.name)
+
+    override fun service(request: HttpRequest, response: HttpResponse) {
+        response.writer.write("Hello World!")
+    }
+}
